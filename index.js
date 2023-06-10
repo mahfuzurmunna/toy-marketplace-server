6038453ffda6266b35eb99acec350c15d884fca4
mahfuzurmunna <mahfuzurrm07@gmail.com>
2023-06-10 23:42:22 +0600

    data post and read done

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // required files
 
-const express = require('express');
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const cors = require('cors');
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,8 +12,6 @@ app.use(cors());
 app.use(express.json());
 
 //mongodb
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rk10a10.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -30,9 +28,39 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const toyCollection = client.db("toyDB").collection("toyDetails");
 
+    // getting data from client to database
+    app.post("/alltoys", async (req, res) => {
+      const receivedToy = req.body;
+      console.log(receivedToy);
+      const result = await toyCollection.insertOne(receivedToy);
 
+      res.send(result);
+    });
 
+    //getting data from database to server side
+    app.get("/alltoys", async (req, res) => {
+      const cursor = toyCollection.find();
+      const result = await cursor.limit(20).toArray();
+      res.send(result);
+    });
+
+    //getting indvidual email data from server to client
+    app.get("/alltoys/:email&:view", async (req, res) => {
+      console.log(req.query);
+      const sellerEmail = req.params.email;
+      const view = req.params.view;
+      console.log(view);
+      const query = {
+        email: sellerEmail
+      };
+      const result = await toyCollection
+        .find(query)
+        .sort({ price: view })
+        .toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -46,13 +74,11 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
 //server starting
 
-app.get('/', (req,res) => {
-  res.send('Toy marketplace server is running')
-})
+app.get("/", (req, res) => {
+  res.send("Toy marketplace server is running");
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
